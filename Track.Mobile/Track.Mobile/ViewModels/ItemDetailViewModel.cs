94dd3037d86d53a4a00f06fd6dabebab54a3faa4
aerdaltr <ahmet.erdal@oztrakya.com>
2019-12-12 16:30:02 +0300

    Proje oluşturuldu.

---
+++ Track.Mobile/Track.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Track.Mobile.Models;
+
+namespace Track.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
